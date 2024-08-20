666817ee15873f260337bb0a5bd42848e7afdb7f
leegunwoooo <leegunwoo0325@gmail.com>
2024-08-20 17:26:55 +0900

    방과후 자바 자료형

---
+++ src/varl.java
@@ -0,0 +1,9 @@
+public class varl {
+    public static void main(String[] args) {
+        //변수 선언
+        int num = 5;
+        //정수형 변수 num = 5
+        System.out.println(num);
+        //5를 출력
+    }
+}

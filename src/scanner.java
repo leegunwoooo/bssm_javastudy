87db4669bd4858424065d13fa488d8bfb1299dab
leegunwoooo <leegunwoo0325@gmail.com>
2024-08-20 17:29:31 +0900

    방과후 자바 스캐너

---
+++ src/scanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        //scanner 사용 뒤엔 무조건 닫아주자 계속 메모리 소모됨
+        System.out.println(num);
+    }
+}

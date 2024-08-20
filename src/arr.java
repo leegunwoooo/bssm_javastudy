6f153b542dfa4f8201f2f74491fa156035397bcd
leegunwoooo <leegunwoo0325@gmail.com>
2024-08-20 19:09:51 +0900

    방과후 자바 배열

---
+++ src/arr.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class arr {
+    public static void main(String[] args) {
+        //배열 c와 같이 같은타입의 자료형을 여러개 저장하기 위해 존재한다
+        //선언 방법은 아래와 같다.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        for(int i=0; i<a; i++){
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        for(int i=0; i<a; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}

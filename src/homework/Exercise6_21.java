97add37b94beb53003321b46c31c9c2eff11182f
leegunwoooo <leegunwoo0325@gmail.com>
2024-08-28 16:09:12 +0900

    자바의 기초 6-21문제

---
+++ src/homework/Exercise6_21.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class Exercise6_21 {
+    static int abs(int value){
+        if(value < 0){
+            return -value;
+        }else{
+            return value;
+        }
+    }
+    public static void main(String[] args) {
+        int value = 5;
+        System.out.println(value + "의 절대값:" + abs(value));
+        value = -10;
+        System.out.println(value + "의 절대값:" + abs(value));
+    }
+}

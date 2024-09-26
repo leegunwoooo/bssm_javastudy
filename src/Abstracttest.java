18fd5729a33ed48a4eea966483232cf79418c912
leegunwoooo <leegunwoo0325@gmail.com>
2024-09-26 18:51:29 +0900

    깃깃

--- src/Abstracttest.java
+++ src/Abstracttest.java
@@ -3,9 +3,9 @@ import Abstractexample.leegunwoo2;
 public class Abstracttest {
     public static void main(String[] args) {
         leegunwoo leegunwoo1 = new leegunwoo();
-        leegunwoo2 leegunwoo2 = new leegunwoo2();
+        leegunwoo2 leegunwoo3 = new leegunwoo2();
         leegunwoo1.study();
-        leegunwoo2.study();
+        leegunwoo3.study();
     }
 }
 

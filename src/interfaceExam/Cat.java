066356a614f2f830c589631d491d8ba2cee419b3
leegunwoooo <leegunwoo0325@gmail.com>
2024-10-28 12:07:59 +0900

    깃깃da

--- src/interfaceExam/Cat.java
+++ src/interfaceExam/Cat.java
@@ -8,7 +8,7 @@ public class Cat implements Animal {
         System.out.println("Eat fish");
     }
     public void Sleep(){
-        System.out.println("dog as cat");
+        System.out.println("dog s cat");
     }}//인터페이스를 상속받은 implements를 사용하면 모든 메서드를 사용해야하지만
         //abstract를 사용하면 메서드를 일부만 사용해도 된다.
 
